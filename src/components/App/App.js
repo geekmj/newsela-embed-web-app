94391e3d440e207aa69c7d61d15fa7f62e1928ad
Garima Magic <garimamagic@GarimaMacbookAir.local>
2020-06-30 13:32:52 +0530

    folder deleted

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import {register} from '../../serviceWorker';
-import Routes from '../../Routes';
+import Routes from '../../routes';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
