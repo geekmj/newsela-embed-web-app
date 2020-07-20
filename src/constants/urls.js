ed93631cadbbbc685ff6266580bda65e2328e82c
Garima Magic <garimamagic@GarimaMacbookAir.local>
2020-07-20 15:52:23 +0530

    server removed.

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,4 @@
 module.exports ={
-    "CHECK_NODE_SERVER": 'http://localhost:4000',
     "SEARCH_API_URL":"https://search.newsela.com/v1",
     "ASSIGNMENT_API_URL":"https://newsela.com/api/v1",
     "NEWSELA_URL":"http://newsela.com"
