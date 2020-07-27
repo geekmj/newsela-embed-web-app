efa6ca8bd55d4d3f5fe304a5785a42f610381b6b
Kazim Raja <kazim.raja@magicsw.com>
2020-07-27 17:15:21 +0530

    BG-2850, BG-2847 and 2852

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,8 +8,8 @@ function Header() {
     return (
         <div className="header">
             <nav className="navbar">
-                <a className="navbar-brand" href="#">
-                    <img src={img} alt='image1' width="35px" height="35px"/>
+                <a className="navbar-brand" href="https://newsela.com/">
+                    <img src={img} alt='logo' width="35px" height="35px"/>
                 </a>
                  <p className="navbar-text">You are selecting content for Canvas</p>
                  <div>
