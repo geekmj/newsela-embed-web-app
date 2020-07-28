9d5c8557d8eb412b84abc9398a488f1ac31d1380
Mrityunjay Kumar <m.kumar1982@gmail.com>
2020-07-28 16:54:20 +0530

    Changes for route and enviornment config
    1. Changed /main to / in routes.js
    2. Introduced env properties files for .env.* for local, qc and prod.
    3. env file will keep API endpoints or URL specific to enviornment
    4. Introduced two build scripts build:qc and build:production
    5. Build script pick env file and replace the value at build in JS HTML
    6. Removed Constant Files url.js and apiEndpoints.js
    7. Removed /config folder
    8. Removed plop file and related folder
    9. Updated README
    10. Make changes in Files to use env based variables.

--- src/config/development.js
+++
@@ -1,5 +0,0 @@
-export default {
-    ENV: 'Dev',
-    API_BASE_URL:'https://magic-consulting.newsela.com',
-    RETURN_URL :" https://newsela.com/apps/lti-tool-provider/universal-embed/finish/"
-}
--- src/config/production.js
+++
@@ -1,5 +0,0 @@
-export default {
-    ENV: 'Prod',
-    API_BASE_URL:'https://magic-consulting.newsela.com',
-    RETURN_URL :" https://newsela.com/apps/lti-tool-provider/universal-embed/finish/"
-}
