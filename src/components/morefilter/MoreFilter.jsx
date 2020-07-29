b433f747322ba9794c8cd5ac6ab20a52a552d355
Anandkar <devone@jetwaystravels.com>
2020-07-29 12:46:37 +0530

    Revert start command & Collection service add

--- src/components/morefilter/MoreFilter.jsx
+++ src/components/morefilter/MoreFilter.jsx
@@ -4,13 +4,16 @@ const MoreFilter = (props) => {
    const filterList = props.filterList;
     return (
          <div className="filter-wrapper">
+            
             <div className="container-fluid my-4 px-4">
+            <form className="dropdownvalue" >
               <div>
                   <div className="button-group">
                      <button className="cancel" onClick={props.cancel}>Cancel</button>
                      <button className="apply">Apply</button>
                   </div> 
                   <div className="row">
+                  
                   {filterList.map((filterItem,index) => (
                      <div className="col-md-6">
                         <h6 className="pt-2">{filterItem.display_name}</h6>
@@ -35,11 +38,12 @@ const MoreFilter = (props) => {
 
                   <div className="button-group">
                      <button className="cancel" onClick={props.cancel}>Cancel</button>
-                     <button className="apply">Apply</button>
+                     <button className="apply" type="submit">Apply</button>
                   </div>   
               </div>    
-
+              </form>
             </div>
+           
          </div>
     )
 }
--- src/services/common.services.js
+++ src/services/common.services.js
@@ -6,3 +6,11 @@ export const searchApi = (searchKeyword, pageNo) => {
     reqObj.method = 'get';
     return apiCaller(reqObj);
 }
+
+
+export const searchCollectionApi = (searchKeyword, pageNo) => {
+    const reqObj = {};
+    reqObj.url = `${process.env.REACT_APP_SEARCH_API_URL}?facets=true&format=full&objects=header&page=${pageNo}&page_size=12${searchKeyword}`;
+    reqObj.method = 'get';
+    return apiCaller(reqObj);
+}
