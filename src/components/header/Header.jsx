af7ded0fb4e6119e90fb232e32304513cfea06c2
Kazim Raja <kazim.raja@magicsw.com>
2020-07-31 15:45:59 +0530

    redesign-header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,15 +13,21 @@ class Header extends Component {
         let launch_by = this.state.queryParams && this.state.queryParams.launch_by
         return (
             <div className="header">
-                <nav className="navbar">
-                    <a className="navbar-brand" href="/">
-                        <img src={img} alt='logo' width="35px" height="35px" />
-                    </a>
-                    <p className="navbar-text">You are selecting content for {launch_by}</p>
-                    <div>
-                        <span className="hide">Exit</span> <FontAwesomeIcon icon={faSignOutAlt} className="signout" />
-                    </div>
-                </nav>
+               <div className="container-fluid">
+                 <div className="row">
+                      <div className="col-2">
+                        <a className="navbar-brand" href="/">
+                            <img src={img} alt='logo' width="35px" height="35px" />
+                        </a>
+                    
+                        </div>
+                        <div className="col-8">
+                         <p className="navbar-text">You are selecting content for {launch_by}</p>
+                        </div>
+                        <div className="col-2">
+                        </div>     
+                 </div> 
+               </div>  
             </div>
         )
     }
