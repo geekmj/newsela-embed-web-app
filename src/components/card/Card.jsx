79dba667b24dea385a4ccbaf4f825f3573cdf092
unknown <magic@MDL-089-CL5Z3W2.magicsw.com>
2020-08-25 19:19:39 +0530

    Code Add

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -57,7 +57,7 @@ class Card extends Component {
       default:
         break;
     }
-     //this.handleHiddenForm(jsonData)
+    this.handleHiddenForm(jsonData)
 
     console.log('SELECTED CARD DATA---->>>>>', JSON.stringify(selectedData))
     console.log('Prepared Respose JSON -------->>>>>>>', JSON.stringify(jsonData))
